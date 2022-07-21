3ca47fdf259338491537584ce13a3671e2323f91
Minh Buu <nguyentominhbuu2002@gmail.com>
2022-07-21 20:28:10 +0700

    service

--- BusinessLayer/OrderServices.cs
+++ BusinessLayer/OrderServices.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Models;
+using DataAccess.Repository;
 
 namespace BusinessLayer;
 
@@ -6,31 +7,81 @@ public class OrderServices : IOrderServices
 {
     public void AddOrder(Order order)
     {
-        throw new NotImplementedException();
+        try
+        {
+            IOrderRepo orderRepo = new OrderRepo();
+            orderRepo.AddOrder(order);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
     public void DeleteOrder(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            IOrderRepo orderRepo = new OrderRepo();
+            orderRepo.DeleteOrder(id);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
     public IEnumerable<Order> GetList()
     {
-        throw new NotImplementedException();
+        try
+        {
+            IOrderRepo orderRepo = new OrderRepo();
+            return orderRepo.GetList();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
     public Order GetOrder(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            IOrderRepo orderRepo = new OrderRepo();
+            return orderRepo.GetOrder(id);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
     public IEnumerable<Order> SearchByDate(DateTime begin, DateTime end)
     {
-        throw new NotImplementedException();
+        try
+        {
+            IOrderRepo orderRepo = new OrderRepo();
+            return from order in orderRepo.GetList()
+                   where order.OrderDate >= begin && order.OrderDate <= end
+                   select order;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
     public void UpdateOrder(Order order)
     {
-        throw new NotImplementedException();
+        try
+        {
+            IOrderRepo orderRepo = new OrderRepo();
+            orderRepo.UpdateOrder(order);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 }
